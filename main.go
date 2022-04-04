cc88bdfafbd979f4eb4602e9d4ee67cdcd602fc6
inobu <inohkunobuya@gmail.com>
2022-04-04 18:45:36 +0900

    test

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	println("test")
 
 	router.GET("/hello", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
